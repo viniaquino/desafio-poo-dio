af184162d329e5337309157fc08e8d4314df07d2
viniaquino <viniaquino@users.noreply.github.com>
2024-09-05 11:24:50 -0300

    Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
         public static void main(String[] args) {
+            Curso curso1 = new Curso();
+            curso1.setTitulo("curso java");
+            curso1.setDescricao("descrição curso java");
+            curso1.setCargaHoraria(8);
 
+            Curso curso2 = new Curso();
+            curso2.setTitulo("curso javascript");
+            curso2.setDescricao("descrição curso javascript");
+            curso2.setCargaHoraria(8);
+
+            Mentoria mentoria = new Mentoria();
+            mentoria.setTitulo("mentoria de java");
+            mentoria.setDescricao("descrição mentoria java");
+            mentoria.setData(LocalDate.now());
+
+            System.out.println(curso1);
+            System.out.println(curso2);
+            System.out.println(mentoria);
         }
-        }
+}
